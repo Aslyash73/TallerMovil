40ee2b761463270849155025cd488806397a73d9
asly cantillo <acantill37@cuc.edu.co>
2023-03-08 17:30:57 -0500

    ajustes design

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -19,60 +19,59 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              'https://th.bing.com/th/id/R.e5d1bf485deeb6d56bdee39f297df56a?rik=k0hvmBtjjqH3Hw&pid=ImgRaw&r=0', // Ruta de la imagen
-              width: 2000.0, // Ancho de la imagen
-              height: 2000.0, // Alto de la imagen
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Hola Asly!',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(height: 20.0),
-            Center(
-              child: ListView.separated(
-                shrinkWrap: true,
-                itemCount: AppRoutes.menuOptions.length,
-                separatorBuilder: (context, index) => const Divider(),
-                itemBuilder: (context, index) {
-                  if (AppRoutes.menuOptions[index].view) {
-                    return ListTile(
-                      title: Text(
-                        AppRoutes.menuOptions[index].name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
+          ),
+          const SizedBox(height: 20.0),
+          Expanded(
+            child: ListView.separated(
+              itemCount: AppRoutes.menuOptions.length,
+              separatorBuilder: (context, index) => const Divider(),
+              itemBuilder: (context, index) {
+                if (AppRoutes.menuOptions[index].view) {
+                  return ListTile(
+                    title: Text(
+                      AppRoutes.menuOptions[index].name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
                       ),
-                      subtitle: Text(
-                        AppRoutes.menuOptions[index].desc,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                        ),
+                    ),
+                    subtitle: Text(
+                      AppRoutes.menuOptions[index].desc,
+                      style: const TextStyle(
+                        fontSize: 16.0,
                       ),
-                      leading: Icon(
-                        AppRoutes.menuOptions[index].icon,
-                        color: Colors.red,
-                      ),
-                      trailing: const Icon(
-                        Icons.navigate_next,
-                        color: Colors.red,
-                      ),
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          AppRoutes.menuOptions[index].route,
-                        );
-                      },
-                    );
-                  } else {
-                    return const SizedBox.shrink();
-                  }
-                },
-              ),
+                    ),
+                    leading: Icon(
+                      AppRoutes.menuOptions[index].icon,
+                      color: Colors.red,
+                    ),
+                    trailing: const Icon(
+                      Icons.navigate_next,
+                      color: Colors.red,
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        AppRoutes.menuOptions[index].route,
+                      );
+                    },
+                  );
+                } else {
+                  return const SizedBox.shrink();
+                }
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
